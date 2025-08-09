f0ed7e8b9bf03a7d7b2044d1ff7358a047023e23
Tiuliugenov <tyulyugenov@gmail.com>
2025-08-09 10:44:40 +0500

    Добавил решенные задачи task 1006 - 1008

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -6,6 +6,8 @@ package com.javarush.task.pro.task10.task1006;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+       ElectricCar electricCar = new ElectricCar();
+       GasCar gasCar = new GasCar();
+       HybridCar hybridCar = new HybridCar();
     }
 }
